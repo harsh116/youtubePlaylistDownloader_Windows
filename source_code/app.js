2fa3a4d6472d1082de540f566f81053e93689a40
harsh116 <71572299+harsh116@users.noreply.github.com>
2022-11-05 16:34:21 +0530

    added source code

---
+++ source_code/app.js
@@ -0,0 +1,241 @@
+const cheerio=require('cheerio')
+const fetch=require('node-fetch')
+
+const fs=require('fs')
+const GetVideo = require("./GetVideo");
+const GetAudio = require("./GetAudio");
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+
+
+
+const VALID_Q=["144",'240','360','480','720','1080']
+
+const Downloader = require("nodejs-file-downloader");
+
+
+
+
+
+const regExURL=/[?:&"\/|]+/g
+
+const getData=async(url)=>{
+
+  
+
+  const response = await fetch(url);
+  const html = await response.text();
+ 
+  const $ = cheerio.load(html);
+ 
+
+  const title = $('meta[property="og:title"]').attr('content') || $('title').text() || $('meta[name="title"]').attr('content')
+  return title
+ 
+}
+
+
+const PORT=8080
+const express=require('express')
+
+const app=express()
+const {scrapePage} = require('./scraper')
+
+const mainDownload=async(playListName,downURL,title,type)=>{
+  let extension=""
+  if(type==='video')
+    extension=".mp4" 
+  else
+    extension=".mp3"
+   const downloader = new Downloader({
+    url: downURL, //If the file name already exists, a new file with the name 200MB1.zip is created.
+    directory: "./"+playListName, //This folder will be created, if it doesn't exist.
+    cloneFiles: false,
+    onError: function (error) {
+    //You can also hook into each failed attempt.
+    console.log("Error from attempt ", error);
+  },
+    fileName: title + extension,
+  });
+  try {
+    const {filePath,downloadStatus} = await downloader.download(); //Downloader.download() resolves with some useful properties.
+
+    console.log("downloadStatus: ",downloadStatus);
+  } catch (error) {
+    //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
+    //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
+    console.log("Download failed", error);
+  }
+}
+
+const main=async(url,q)=>{
+
+
+let playListName= await getData(url)
+if(!playListName || playListName=='null')
+{
+  console.log('Error in accesing playlist. Make sure to have your playlist visibility is unlisted or public')
+  return
+}
+playListName=playListName.replace(regExURL," ")
+console.log('playlist name: ' ,playListName)
+
+
+
+
+const list= await scrapePage(url)
+let i=1
+
+ if(!q || !VALID_Q.includes(q))
+ {
+  q="480"
+ }
+
+
+for(let lis of list)
+{
+ 
+  
+    GetVideo(lis,q)
+    .then(async(data) => {
+    let title=data.title
+    title=title.replace(regExURL," ")
+  
+    const downURL=data.urlDown
+    console.log(i,': ',downURL)
+    i++
+
+    if(downURL==='Error')
+    {
+      console.log('deteced',title) 
+      return
+    }
+
+   mainDownload(playListName,downURL,title,"video")
+
+
+
+
+})
+
+}
+
+}
+
+if (!fs.existsSync('./playlist')){
+  fs.mkdirSync('./playlist')
+  fs.writeFileSync('playlist/playlist.txt',"")
+  fs.writeFileSync('playlist/quality.txt',"")
+  fs.writeFileSync('playlist/playlist_audio.txt',"")
+}
+
+let file
+try{
+  file=fs.readdirSync('playlist')
+  
+}
+catch(err)
+{ 
+  console.log('err: ',err)
+}
+
+
+const downloadAudio=async(url)=>
+{
+
+  let playListName= await getData(url)
+  if(!playListName || playListName=='null')
+  {
+    console.log('Error in accesing playlist. Make sure to have your playlist visibility is unlisted or public')
+    return
+  }
+  playListName=playListName.replace(regExURL," ")
+  console.log('playlist name: ' ,playListName)
+
+
+
+
+  const list= await scrapePage(url)
+  let i=1
+
+  
+
+  for(let lis of list)
+  {
+   
+    
+      GetAudio(lis)
+      .then(async(data) => {
+      let title=data.title
+      title=title.replace(regExURL," ")
+    
+      const downURL=data.urlDown
+      console.log(i,': ',downURL)
+      i++
+
+      if(downURL==='Error')
+      {
+        console.log('deteced',title) 
+        return
+      }
+
+     mainDownload(playListName,downURL,title,"audio")
+
+
+
+
+  })
+
+  }
+}
+
+const str1=fs.readFileSync('playlist/'+file[0]).toString()
+const str3=fs.readFileSync('playlist/'+'playlist_audio.txt').toString()
+
+if(str3.length>0)
+{
+  const list=str3.split(/\s+/g)
+  for(let lis of list)
+  {
+    if(lis.length==0||!(lis.length>4&&lis.substr(0,4).toLowerCase()==='http'))
+    {
+      console.log('Invalid url')
+      continue;
+    }
+    console.log('lis: ',lis)
+    downloadAudio(lis)
+    
+  }
+}
+
+if(str1.length==0)
+{
+  console.log('Please start typing playlist links at playlist/playlist.txt separated by space or new line')
+}
+else{
+  const str2=fs.readFileSync('playlist/'+file[1]).toString()
+  const list1=str1.split(/\s+/g)
+  const list2=str2.split(/\s+/g)
+
+
+
+  let x=0
+  for(let lis of list1)
+  {
+    if(lis.length==0||!(lis.length>4&&lis.substr(0,4).toLowerCase()==='http'))
+    {
+      console.log('Invalid url')
+      continue;
+    }
+    console.log('lis: ',lis)
+    main(lis,list2[x])
+    x++
+  }
+}
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`app is running on port ${PORT}`);
+})
---
+++ source_code/scraper.js
@@ -0,0 +1,98 @@
+ const puppeteer=require('puppeteer')
+ var path = require('path');
+ 
+// if (process.pkg) {
+//   var puppeteer = require(path.resolve(process.cwd(), 'puppeteer'));
+// } else {
+//   var puppeteer = require('puppeteer');
+// }
+ 
+ 
+ async function getElText(page, selector) {
+     return await page.evaluate((selector) => {
+         return document.querySelector(selector).innerText
+     }, selector);
+ }
+ 
+ async function scrapePage(url) {
+     // const browser = await puppeteer.launch({executablePath: 'node_modules/chromium/lib/chromium/chrome-win/Chrome'});
+     const browser = await puppeteer.launch({ executablePath: './chromium/chrome-win/chrome.exe'});
+     const page = await browser.newPage();
+     const totalStart = Date.now();
+ 
+     await page.goto(url);
+     var elements;
+     var goAgain = true;
+     while (goAgain) {
+         const divCount = await page.$$eval('paper-spinner', divs => divs.length);
+         await page.evaluate(async() => {
+             window.scrollTo(0, document.querySelector("#primary").scrollHeight)
+         })
+         goAgain = divCount != 0;
+     }
+     elements = await page.$$('a.ytd-playlist-video-renderer');
+     var len = elements.length
+     console.log(`Items in playlist: ${len}`);
+
+     // elements=await page.$$('video')
+     // const len=elements.length
+    
+    const list=[]
+     for (var i = 0; i < len; i++) {
+         const elem = elements[i];
+         // const href=elem
+         
+         const href = await page.evaluate(e => e.href, elem); //Chrome will return the absolute URL
+         list.push(href)
+         // const newPage = await browser.newPage();
+     
+         // //Following lines block media to speed up scrape
+         // await newPage.setRequestInterception(true);
+ 
+         // newPage.on('request', request => {
+         //         const url = request.url().toLowerCase()
+         //         const resourceType = request.resourceType()
+ 
+         //         if (resourceType == 'media' ||
+         //             url.endsWith('.mp4') ||
+         //             url.endsWith('.avi') ||
+         //             url.endsWith('.flv') ||
+         //             url.endsWith('.mov') ||
+         //             url.endsWith('.wmv') ||
+         //             url.includes('videoplayback')) {
+         //             request.abort();
+         //         } else
+         //             request.continue();
+         //     })
+             
+         // const start = Date.now();
+         // await newPage.goto(href);;
+         // try {
+         //     await newPage.waitForSelector('.short-view-count.yt-view-count-renderer');
+         //     const viewCount = await getElText(newPage, '.view-count.yt-view-count-renderer');
+         //     const titleText = await getElText(newPage, '.title.ytd-video-primary-info-renderer');
+         //     const uploadDate = await getElText(newPage, '#date yt-formatted-string.ytd-video-primary-info-renderer');
+ 
+         //     console.log('Title: ', titleText)
+         //     console.log(viewCount)
+         //     console.log('Upload date: ', uploadDate)
+         //     // console.log('Loaded in', Date.now() - start, 'ms')
+         //     await newPage.close();
+         // } catch (error) {
+         //     console.log("UNSUCCESSFUL SCRAPE! Video may be private or deleted.")
+         // }
+     }
+     browser.close();
+     var end = new Date().getTime();
+     var time = end - totalStart;
+     var shortTime = (time/60000).toFixed(3);
+     console.log(len + ' items scraped in ' + time/1000 + " seconds!" + " (" + shortTime + ") minutes.")
+     return list
+ ;
+}
+
+ module.exports ={
+    scrapePage
+ }
+ 
+ 
