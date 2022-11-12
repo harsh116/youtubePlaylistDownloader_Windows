7d85dd532984e0067d7483fc8ff4cb8f27e15110
harsh116 <71572299+harsh116@users.noreply.github.com>
2022-11-12 19:30:27 +0530

    cache implementation and unlimited videos in single playlist instead of first 100

--- source_code/app.js
+++ source_code/app.js
@@ -2,6 +2,19 @@ const cheerio = require("cheerio");
 const fetch = require("node-fetch");
 
 const fs = require("fs");
+const FileSystemCache_1 = require("file-system-cache");
+
+const playlistoptions = {
+  basePath: "./.cache/playlist", // Optional. Path where cache files are stored (default).
+  ns: "playlist", // Optional. A grouping namespace for items.
+};
+const videoOptions = {
+  basePath: "./.cache/videos", // Optional. Path where cache files are stored (default).
+  ns: "videos", // Optional. A grouping namespace for items.
+};
+const playlistCache = new FileSystemCache_1.FileSystemCache(playlistoptions);
+const videoCache = new FileSystemCache_1.FileSystemCache(videoOptions);
+
 const GetVideo = require("./GetVideo");
 const GetAudio = require("./GetAudio");
 
@@ -31,38 +44,9 @@ const express = require("express");
 
 const app = express();
 const { scrapePage } = require("./scraper");
+// const { title } = require("process");
 
-const mainDownload =  (playListName, downURL, title, type) => {
-  return new Promise(async (resolve, reject) => {
-  let extension = "";
-  if (type === "video") extension = ".mp4";
-  else extension = ".mp3";
-  const downloader = new Downloader({
-    url: downURL, //If the file name already exists, a new file with the name 200MB1.zip is created.
-    directory: "./" + playListName, //This folder will be created, if it doesn't exist.
-    cloneFiles: false,
-    onError: function (error) {
-      //You can also hook into each failed attempt.
-      console.log("Error from attempt ", error);
-    },
-    fileName: title + extension,
-  });
-  try {
-    const { filePath, downloadStatus } = await downloader.download(); //Downloader.download() resolves with some useful properties.
-
-    resolve(downloadStatus)
-
-  } catch (error) {
-    //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
-    //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
-    // console.log("Download failed", error);
-    reject(error)
-  }
-})
-  
-};
-
-const main = async (url, q) => {
+const gettingVideosURL = async (url) => {
   let playListName = await getData(url);
   if (!playListName || playListName == "null") {
     console.log(
@@ -72,52 +56,99 @@ const main = async (url, q) => {
   }
   playListName = playListName.replace(regExURL, " ");
   console.log("playlist name: ", playListName);
+  let list = [];
+  if (await playlistCache.fileExists(url)) {
+    list = await playlistCache.get(url);
+  } else {
+    list = await scrapePage(url);
+    await playlistCache.set(url, list);
+  }
 
-  const list = await scrapePage(url);
+  return { list, playListName };
+};
+
+const mainDownload = (playListName, downURL, title, type) => {
+  return new Promise(async (resolve, reject) => {
+    let extension = "";
+    if (type === "video") extension = ".mp4";
+    else extension = ".mp3";
+    const downloader = new Downloader({
+      url: downURL, //If the file name already exists, a new file with the name 200MB1.zip is created.
+      directory: "./" + playListName, //This folder will be created, if it doesn't exist.
+      cloneFiles: false,
+      onError: function (error) {
+        //You can also hook into each failed attempt.
+        console.log("Error from attempt ", error);
+      },
+      fileName: title + extension,
+    });
+    try {
+      const { filePath, downloadStatus } = await downloader.download(); //Downloader.download() resolves with some useful properties.
+
+      resolve(downloadStatus);
+    } catch (error) {
+      //IMPORTANT: Handle a possible error. An error is thrown in case of network errors, or status codes of 400 and above.
+      //Note that if the maxAttempts is set to higher than 1, the error is thrown only if all attempts fail.
+      // console.log("Download failed", error);
+      reject(error);
+    }
+  });
+};
+
+const main = async (url, q) => {
+  const { list, playListName } = await gettingVideosURL(url);
+
   let i = 1;
 
   if (!q || !VALID_Q.includes(q)) {
     q = "480";
   }
 
-  const videoList=[]
+  const videoList = [];
   for (let lis of list) {
-    const data=await GetVideo(lis, q)
+    let title = await getData(lis);
+    title = title.replace(regExURL, " ");
+
+    const isExist = fs.existsSync(`${playListName}/${title}.mp4`);
+    if (isExist) {
+      console.log(`${title} already exist`);
+      continue;
+    }
+
+    let data = {};
+
+    if (await videoCache.fileExists(lis)) {
+      data = await videoCache.get(lis);
+    } else {
+      data = await GetVideo(lis, q);
+      videoCache.set(lis, data);
+    }
     // .then(async (data) => {
-      let title = data.title;
-      title = title.replace(regExURL, " ");
+    // let title = data.title;
 
-      const isExist = fs.existsSync(`${playListName}/${title}.mp4`);
-      if (isExist) {
-        console.log(`${title} already exist`);
-        continue;
-      }
+    const downURL = data.urlDown;
+    console.log(i, ": ", downURL);
+    i++;
 
-      const downURL = data.urlDown;
-      console.log(i, ": ", downURL);
-      i++;
+    if (downURL === "Error") {
+      console.log("deteced", title);
+      continue;
+    }
 
-      if (downURL === "Error") {
-        console.log("deteced", title);
-        continue;
-      }
-      videoList.push({downURL,title})
-      // await mainDownload(playListName, downURL, title, "video");
+    try {
+      const res = await mainDownload(playListName, downURL, title, "video");
+      console.log("completed");
+    } catch (err) {
+      console.log("error: ", err);
+    }
+    // videoList.push({ downURL, title });
+    // await mainDownload(playListName, downURL, title, "video");
     // });
   }
   // console.log('videoList: ',videoList)
-  for(let videoli of videoList)
-  {
-    try{
-      const res=await mainDownload(playListName,videoli.downURL,videoli.title,'video')
-      console.log('completed')
-    }
-    catch(err){
-      console.log('error: ',err)
-    }
-    
-  }
+  // for (let videoli of videoList) {
 
+  // }
 };
 
 if (!fs.existsSync("./playlist")) {
@@ -125,6 +156,17 @@ if (!fs.existsSync("./playlist")) {
   fs.writeFileSync("playlist/playlist.txt", "");
   fs.writeFileSync("playlist/quality.txt", "");
   fs.writeFileSync("playlist/playlist_audio.txt", "");
+  fs.writeFileSync("playlist/clear_cache.txt", "");
+}
+
+const cacheClearStatus = fs.readFileSync("playlist/clear_cache.txt").toString();
+
+if (
+  cacheClearStatus.length >= 1 &&
+  (cacheClearStatus[0] === "y" || cacheClearStatus[0] === "Y")
+) {
+  playlistCache.clear();
+  videoCache.clear();
 }
 
 let file;
@@ -135,59 +177,54 @@ try {
 }
 
 const downloadAudio = async (url) => {
-  let playListName = await getData(url);
-  if (!playListName || playListName == "null") {
-    console.log(
-      "Error in accesing playlist. Make sure to have your playlist visibility is unlisted or public"
-    );
-    return;
-  }
-  playListName = playListName.replace(regExURL, " ");
-  console.log("playlist name: ", playListName);
-
-  const list = await scrapePage(url);
+  const { list, playListName } = await gettingVideosURL(url);
   let i = 1;
 
-  const audioList=[]
+  const audioList = [];
   for (let lis of list) {
-    const data=await GetAudio(lis)
+    let title = "";
+    try {
+      title = await getData(lis);
+    } catch (err) {
+      console.error(err);
+    }
+    title = title.replace(regExURL, " ");
+
+    const isExist = fs.existsSync(`${playListName}/${title}.mp3`);
+    if (isExist) {
+      console.log(`${title} already exist`);
+      continue;
+    }
+    const data = await GetAudio(lis);
     // .then(async (data) => {
-      let title = data.title;
-      title = title.replace(regExURL, " ");
+    // let title = data.title;
 
-      const isExist = fs.existsSync(`${playListName}/${title}.mp3`);
-      if (isExist) {
-        console.log(`${title} already exist`);
-        continue;
-      }
+    const downURL = data.urlDown;
+    console.log(i, ": ", downURL);
+    i++;
 
-      const downURL = data.urlDown;
-      console.log(i, ": ", downURL);
-      i++;
+    if (downURL === "Error") {
+      console.log("deteced", title);
+      continue;
+    }
 
-      if (downURL === "Error") {
-        console.log("deteced", title);
-        continue;
-      }
+    try {
+      const res = await mainDownload(playListName, downURL, title, "audio");
+      console.log("completed");
+    } catch (err) {
+      console.log("error: ", err);
+    }
 
-      audioList.push({downURL,title})
+    // audioList.push({ downURL, title });
     // });
   }
 
-  for(let audioLi of audioList)
-  {
-    try{
-      const res=await mainDownload(playListName,audioli.downURL,audioli.title,'audio')
-      console.log('completed')
-    }
-    catch(err){
-      console.log('error: ',err)
-    }
-  }
-  
+  // for (let audioLi of audioList) {
+
+  // }
 };
 
-const str1 = fs.readFileSync("playlist/" + file[0]).toString();
+const str1 = fs.readFileSync("playlist/" + "playlist.txt").toString();
 const str3 = fs.readFileSync("playlist/" + "playlist_audio.txt").toString();
 
 if (str3.length > 0) {
@@ -210,7 +247,7 @@ if (str1.length == 0) {
     "Please start typing playlist links at playlist/playlist.txt separated by space or new line"
   );
 } else {
-  const str2 = fs.readFileSync("playlist/" + file[1]).toString();
+  const str2 = fs.readFileSync("playlist/" + "quality.txt").toString();
   const list1 = str1.split(/\s+/g);
   const list2 = str2.split(/\s+/g);
 
--- source_code/scraper.js
+++ source_code/scraper.js
@@ -1,98 +1,150 @@
- const puppeteer=require('puppeteer')
- var path = require('path');
- 
+const puppeteer = require("puppeteer");
+// var path = require("path");
+// const { resolve } = require("path");
+
+const promiseSetTimeOut = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+};
+
 // if (process.pkg) {
 //   var puppeteer = require(path.resolve(process.cwd(), 'puppeteer'));
 // } else {
 //   var puppeteer = require('puppeteer');
 // }
- 
- 
- async function getElText(page, selector) {
-     return await page.evaluate((selector) => {
-         return document.querySelector(selector).innerText
-     }, selector);
- }
- 
- async function scrapePage(url) {
-     // const browser = await puppeteer.launch({executablePath: 'node_modules/chromium/lib/chromium/chrome-win/Chrome'});
-     const browser = await puppeteer.launch({ executablePath: './chromium/chrome-win/chrome.exe'});
-     const page = await browser.newPage();
-     const totalStart = Date.now();
- 
-     await page.goto(url);
-     var elements;
-     var goAgain = true;
-     while (goAgain) {
-         const divCount = await page.$$eval('paper-spinner', divs => divs.length);
-         await page.evaluate(async() => {
-             window.scrollTo(0, document.querySelector("#primary").scrollHeight)
-         })
-         goAgain = divCount != 0;
-     }
-     elements = await page.$$('a.ytd-playlist-video-renderer');
-     var len = elements.length
-     console.log(`Items in playlist: ${len}`);
 
-     // elements=await page.$$('video')
-     // const len=elements.length
-    
-    const list=[]
-     for (var i = 0; i < len; i++) {
-         const elem = elements[i];
-         // const href=elem
-         
-         const href = await page.evaluate(e => e.href, elem); //Chrome will return the absolute URL
-         list.push(href)
-         // const newPage = await browser.newPage();
-     
-         // //Following lines block media to speed up scrape
-         // await newPage.setRequestInterception(true);
- 
-         // newPage.on('request', request => {
-         //         const url = request.url().toLowerCase()
-         //         const resourceType = request.resourceType()
- 
-         //         if (resourceType == 'media' ||
-         //             url.endsWith('.mp4') ||
-         //             url.endsWith('.avi') ||
-         //             url.endsWith('.flv') ||
-         //             url.endsWith('.mov') ||
-         //             url.endsWith('.wmv') ||
-         //             url.includes('videoplayback')) {
-         //             request.abort();
-         //         } else
-         //             request.continue();
-         //     })
-             
-         // const start = Date.now();
-         // await newPage.goto(href);;
-         // try {
-         //     await newPage.waitForSelector('.short-view-count.yt-view-count-renderer');
-         //     const viewCount = await getElText(newPage, '.view-count.yt-view-count-renderer');
-         //     const titleText = await getElText(newPage, '.title.ytd-video-primary-info-renderer');
-         //     const uploadDate = await getElText(newPage, '#date yt-formatted-string.ytd-video-primary-info-renderer');
- 
-         //     console.log('Title: ', titleText)
-         //     console.log(viewCount)
-         //     console.log('Upload date: ', uploadDate)
-         //     // console.log('Loaded in', Date.now() - start, 'ms')
-         //     await newPage.close();
-         // } catch (error) {
-         //     console.log("UNSUCCESSFUL SCRAPE! Video may be private or deleted.")
-         // }
-     }
-     browser.close();
-     var end = new Date().getTime();
-     var time = end - totalStart;
-     var shortTime = (time/60000).toFixed(3);
-     console.log(len + ' items scraped in ' + time/1000 + " seconds!" + " (" + shortTime + ") minutes.")
-     return list
- ;
+async function getElText(page, selector) {
+  return await page.evaluate((selector) => {
+    return document.querySelector(selector).innerText;
+  }, selector);
 }
 
- module.exports ={
-    scrapePage
- }
- 
- 
+const autoScroll = async (page) => {
+  // console.log(document, window);
+  // while(
+  await page.evaluate(async () => {
+    // console.log(document, window);
+    await new Promise((resolve) => {
+      var totalHeight = 0;
+      var distance = 100;
+      // console.log(document, window);
+      // while (true) {
+      var timer = setInterval(() => {
+        // console.log(document, window);
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 5000);
+      // }
+    });
+  });
+  // )
+};
+
+async function scrapePage(url) {
+  // const browser = await puppeteer.launch({executablePath: 'node_modules/chromium/lib/chromium/chrome-win/Chrome'});
+  const browser = await puppeteer.launch({
+    executablePath: "./chromium/chrome-win/chrome.exe",
+    // headless: false,
+  });
+  const page = await browser.newPage();
+  const totalStart = Date.now();
+
+  await page.goto(url);
+  // await page.setViewport({
+  //   width: 1200,
+  //   height: 800,
+  // });
+  // await autoScroll(page);
+
+  var elements;
+  var goAgain = true;
+  while (goAgain) {
+    const divCount = await page.$$eval(
+      "tp-yt-paper-spinner",
+      (divs) => divs.length
+    );
+    await page.evaluate(async () => {
+      window.scrollTo(0, document.querySelector("#primary").scrollHeight);
+    });
+    goAgain = divCount != 0;
+  }
+  elements = await page.$$("a.ytd-playlist-video-renderer");
+  var len = elements.length;
+  console.log(`Items in playlist: ${len}`);
+
+  // elements=await page.$$('video')
+  // const len=elements.length
+
+  const list = [];
+  for (var i = 0; i < len; i++) {
+    const elem = elements[i];
+    // const href=elem
+
+    const href = await page.evaluate((e) => e.href, elem); //Chrome will return the absolute URL
+    list.push(href);
+    // const newPage = await browser.newPage();
+
+    // //Following lines block media to speed up scrape
+    // await newPage.setRequestInterception(true);
+
+    // newPage.on('request', request => {
+    //         const url = request.url().toLowerCase()
+    //         const resourceType = request.resourceType()
+
+    //         if (resourceType == 'media' ||
+    //             url.endsWith('.mp4') ||
+    //             url.endsWith('.avi') ||
+    //             url.endsWith('.flv') ||
+    //             url.endsWith('.mov') ||
+    //             url.endsWith('.wmv') ||
+    //             url.includes('videoplayback')) {
+    //             request.abort();
+    //         } else
+    //             request.continue();
+    //     })
+
+    // const start = Date.now();
+    // await newPage.goto(href);;
+    // try {
+    //     await newPage.waitForSelector('.short-view-count.yt-view-count-renderer');
+    //     const viewCount = await getElText(newPage, '.view-count.yt-view-count-renderer');
+    //     const titleText = await getElText(newPage, '.title.ytd-video-primary-info-renderer');
+    //     const uploadDate = await getElText(newPage, '#date yt-formatted-string.ytd-video-primary-info-renderer');
+
+    //     console.log('Title: ', titleText)
+    //     console.log(viewCount)
+    //     console.log('Upload date: ', uploadDate)
+    //     // console.log('Loaded in', Date.now() - start, 'ms')
+    //     await newPage.close();
+    // } catch (error) {
+    //     console.log("UNSUCCESSFUL SCRAPE! Video may be private or deleted.")
+    // }
+  }
+  browser.close();
+  var end = new Date().getTime();
+  var time = end - totalStart;
+  var shortTime = (time / 60000).toFixed(3);
+  console.log(
+    len +
+      " items scraped in " +
+      time / 1000 +
+      " seconds!" +
+      " (" +
+      shortTime +
+      ") minutes."
+  );
+  return list;
+}
+
+module.exports = {
+  scrapePage,
+};
